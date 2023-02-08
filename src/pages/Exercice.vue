3bb4ad10f2c67a5222c7ed1a92a37c6a63acd0f2
Chappuis Flavien <75837575+Flavien-dev@users.noreply.github.com>
2023-02-08 15:18:51 +0100

    ajout du getter dynamique au lieu du tableau static

--- src/pages/Exercice.vue
+++ src/pages/Exercice.vue
@@ -49,43 +49,19 @@ Exercice 3 - Vuex & Formulaires
 </template>
 
 <script>
+// importation des getters du magasin
+import { mapGetters } from 'vuex'
+
 export default {
   data () {
     return {
       // TODO utiliser le getters du magasin pour récupérer les plats
-      plats: [
-        {
-          id: 1,
-          image: 'https://i.imgur.com/0umadnY.jpg',
-          nom: 'Burger',
-          description: "Un hamburger est un sandwich composé d'une ou plusieurs tranches de viande hachée, généralement du bœuf, placées dans un petit pain ou une brioche.",
-          note: 4
-        },
-        {
-          id: 2,
-          image: 'https://i.imgur.com/b9zDbyb.jpg',
-          nom: 'Pizza',
-          description: "La pizza est un plat savoureux d'origine italienne, consistant en une base généralement ronde et aplatie de pâte levée à base de blé.",
-          note: 5
-        },
-        {
-          id: 3,
-          image: 'https://i.imgur.com/RbKjUjB.jpg',
-          nom: 'Petits choux',
-          description:
-              'Le chou de Bruxelles est une variété de chou, plante herbacée de la famille des Brassicaceae. C’est vraiement pas bon...',
-          note: 1
-        },
-        {
-          id: 4,
-          image: 'https://i.imgur.com/xAuhNVg.jpg',
-          nom: 'BBQ Ribs',
-          description: 'Les BBQ ribs ou barbecue ribs sont des grands classiques très appréciés partout dans le monde.',
-          note: 5
-        }
-      ],
       afficherFormPlat: false
     }
+  },
+  computed: {
+    // récupère le getter de plats
+    ...mapGetters('plats', ['plats'])
   },
   components: {
     plat: require('components/Plat.vue').default,
